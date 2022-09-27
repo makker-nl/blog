c98d1ea5c4badb66b89bd48bb78579760b647f88
Martien van den Akker <martien.van.den.akker@virtualsciences.nl>
2022-09-27 15:58:27 +0200

    Add support AMQP

--- SoapUI/keystore_env.bat
+++ SoapUI/keystore_env.bat
@@ -4,9 +4,9 @@
 @set DNAME="CN=Martien van den Akker, OU=VS, O=VirtualSciences, L=Nieuwegein, ST=Utrecht, C=NL, EmailAddress=makker@vs.local"
 @set VALIDITY=365
 @set KEYSIZE=2048
-@set CERT_HOME="certificaat"
-@set ROOT_CERT_HOME="certificaat"
+@set CERT_HOME="certificates"
+@set ROOT_CERT_HOME="certificates"
 @set ROOT_CERT_1=CACertificate.cer
 @set ROOT_CERT_ALIAS_1=CACertificate
-@set ROOT_CERT_2=SSC-I-RootCACertificate.cer
-@set ROOT_CERT_ALIAS_2=RootCACertificate
+#@set ROOT_CERT_2=SSC-I-RootCACertificate.cer
+#@set ROOT_CERT_ALIAS_2=RootCACertificate
